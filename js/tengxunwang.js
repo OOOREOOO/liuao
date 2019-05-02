05abb255243f58c7f91422e9b46c797f8751bde1
OOOREOOO <834060266@qq.com>
2019-05-02 17:04:14 +0800

    update

---
+++ js/tengxunwang.js
@@ -0,0 +1,11 @@
+alert("hello");
+// var ad = document.getElementById("ad2");
+// function change(n){
+//   if (n>36) n=1;
+//   ad.setAttribute("src",n+".jpg");
+//   n++;
+//   setTimeout("change("+n+")",1000);
+// }
+// widows.download = function(){
+//   setTimeout("change(1)", 1000);
+// }
