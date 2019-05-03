9309535e1a376842698777a0f58e32de75706d9a
OOOREOOO <834060266@qq.com>
2019-05-03 15:10:27 +0800

    update

--- js/tengxunwang.js
+++ js/tengxunwang.js
@@ -1,11 +1,24 @@
-alert("hello");
-// var ad = document.getElementById("ad2");
-// function change(n){
-//   if (n>36) n=1;
-//   ad.setAttribute("src",n+".jpg");
-//   n++;
-//   setTimeout("change("+n+")",1000);
-// }
-// widows.download = function(){
-//   setTimeout("change(1)", 1000);
-// }
+
+var title1 = document.getElementById("title_1");
+var title2 = document.getElementById("title_2");
+
+
+title1.onmouseover = function t1(){
+  title1.className = "title_1 actived";
+  if(title2.className = "title_2 actived"){
+    title2.className = "title_2"
+  }
+}
+title2.onmouseover = function t2(){
+  title2.className = "title_2 actived";
+  if(title1.className = "title_1 actived"){
+    title1.className = "title_1"
+  }
+}
+function finish(){
+  alert("JavaScript加载完成");
+}
+
+window.onload = function() {
+  t1(); t2(); finish();
+}
